76b2d375dad13f1850465df57f975ce87e78cbdf
TheSpeedX <ggspeedx29@gmail.com>
2022-04-09 18:25:48 +0530

    add messages

--- lib/constant.dart
+++ lib/constant.dart
@@ -14,3 +14,15 @@ const primaryGradientColor = LinearGradient(
 );
 const secondaryColor = Color(0xFF979797);
 const textColor = Color(0xFF757575);
+
+
+final RegExp emailValidatorRegExp =
+    RegExp(r"^[a-zA-Z0-9.]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
+const String emailNullError = "Please Enter your email";
+const String invalidEmailError = "Please Enter Valid Email";
+const String passNullError = "Please Enter your password";
+const String shortPassError = "Password is too short";
+const String matchPassError = "Passwords don't match";
+const String namelNullError = "Please Enter your name";
+const String phoneNumberNullError = "Please Enter your phone number";
+const String addressNullError = "Please Enter your address";
