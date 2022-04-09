a2537e150d4a79d8cb310a1a3ac166254e6e746b
Sourav Bera <sauravberaiit@gmail.com>
2022-04-09 22:17:56 +0530

    added loading cursor

---
+++ lib/components/loading_dialog.dart
@@ -0,0 +1,22 @@
+import 'package:ev_station/components/progress_bar.dart';
+import 'package:flutter/material.dart';
+
+class LoadingDialog extends StatelessWidget {
+  final String? message;
+
+  const LoadingDialog({Key? key, this.message}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      key: key,
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          circularProgress(),
+          const SizedBox(height: 15.0),
+          Text(message! + "Please Wait.."),
+        ],
+      ),
+    );
+  }
+}
