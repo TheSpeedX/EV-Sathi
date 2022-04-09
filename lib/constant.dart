ec42d9c03fad8abb35692dd6bfbdd57954681043
TheSpeedX <ggspeedx29@gmail.com>
2022-04-09 17:56:41 +0530

    add color

---
+++ lib/constant.dart
@@ -0,0 +1,16 @@
+import 'package:ev_station/size_config.dart';
+import 'package:flutter/material.dart';
+
+
+const primaryColor = Color.fromARGB(15, 75, 21, 161);
+const primaryLightColor = Color(0xFFFECDEF);
+const primaryGradientColor = LinearGradient(
+  begin: Alignment.topRight,
+  end: Alignment.bottomRight,
+  colors: [
+    Color(0xFFFFA53E),
+    Color(0xFFFF7643),
+  ],
+);
+const secondaryColor = Color(0xFF979797);
+const textColor = Color(0xFF757575);
