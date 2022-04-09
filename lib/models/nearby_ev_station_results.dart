5589aa8dcd36357d7728c14189f613a5532abcde
Sourav Bera <sauravberaiit@gmail.com>
2022-04-09 21:48:52 +0530

    added model file nearby ev station search results

---
+++ lib/models/nearby_ev_station_results.dart
@@ -0,0 +1,232 @@
+import 'package:http/http.dart' as http;
+
+class NearbyEVStationResults {
+  String? businessStatus;
+  Geometry? geometry;
+  String? icon;
+  String? iconBackgroundColor;
+  String? iconMaskBaseUri;
+  String? name;
+  String? placeId;
+  PlusCode? plusCode;
+  // ignore: prefer_typing_uninitialized_variables
+  var rating;
+  String? reference;
+  String? scope;
+  List<String>? types;
+    // ignore: prefer_typing_uninitialized_variables
+  var userRatingsTotal;
+  String? vicinity;
+  List<Photos>? photos;
+  OpeningHours? openingHours;
+
+  NearbyEVStationResults(
+      {this.businessStatus,
+      this.geometry,
+      this.icon,
+      this.iconBackgroundColor,
+      this.iconMaskBaseUri,
+      this.name,
+      this.placeId,
+      this.plusCode,
+      this.rating,
+      this.reference,
+      this.scope,
+      this.types,
+      this.userRatingsTotal,
+      this.vicinity,
+      this.photos,
+      this.openingHours});
+
+  NearbyEVStationResults.fromJson(Map<dynamic, dynamic> json) {
+    businessStatus = json['business_status'];
+    geometry = json['geometry'] != null
+        ? Geometry.fromJson(json['geometry'])
+        : null;
+    icon = json['icon'];
+    iconBackgroundColor = json['icon_background_color'];
+    iconMaskBaseUri = json['icon_mask_base_uri'];
+    name = json['name'];
+    placeId = json['place_id'];
+    plusCode = json['plus_code'] != null
+        ? PlusCode.fromJson(json['plus_code'])
+        : null;
+    rating = json['rating'];
+    reference = json['reference'];
+    scope = json['scope'];
+    types = json['types'].cast<String>();
+    userRatingsTotal = json['user_ratings_total'];
+    vicinity = json['vicinity'];
+    if (json['photos'] != null) {
+      photos = <Photos>[];
+      json['photos'].forEach((v) {
+        photos!.add(Photos.fromJson(v));
+      });
+    }
+    openingHours = json['opening_hours'] != null
+        ? OpeningHours.fromJson(json['opening_hours'])
+        : null;
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = Map<String, dynamic>();
+    data['business_status'] = this.businessStatus;
+    if (this.geometry != null) {
+      data['geometry'] = this.geometry!.toJson();
+    }
+    data['icon'] = this.icon;
+    data['icon_background_color'] = this.iconBackgroundColor;
+    data['icon_mask_base_uri'] = this.iconMaskBaseUri;
+    data['name'] = this.name;
+    data['place_id'] = this.placeId;
+    if (this.plusCode != null) {
+      data['plus_code'] = this.plusCode!.toJson();
+    }
+    data['rating'] = this.rating;
+    data['reference'] = this.reference;
+    data['scope'] = this.scope;
+    data['types'] = this.types;
+    data['user_ratings_total'] = this.userRatingsTotal;
+    data['vicinity'] = this.vicinity;
+    if (this.photos != null) {
+      data['photos'] = this.photos!.map((v) => v.toJson()).toList();
+    }
+    if (this.openingHours != null) {
+      data['opening_hours'] = this.openingHours!.toJson();
+    }
+    return data;
+  }
+}
+
+class Geometry {
+  Location? location;
+  Viewport? viewport;
+
+  Geometry({this.location, this.viewport});
+
+  Geometry.fromJson(Map<String, dynamic> json) {
+    location = json['location'] != null
+        ? Location.fromJson(json['location'])
+        : null;
+    viewport = json['viewport'] != null
+        ? Viewport.fromJson(json['viewport'])
+        : null;
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = Map<String, dynamic>();
+    if (this.location != null) {
+      data['location'] = this.location!.toJson();
+    }
+    if (this.viewport != null) {
+      data['viewport'] = this.viewport!.toJson();
+    }
+    return data;
+  }
+}
+
+class Location {
+  double? lat;
+  double? lng;
+
+  Location({this.lat, this.lng});
+
+  Location.fromJson(Map<String, dynamic> json) {
+    lat = json['lat'];
+    lng = json['lng'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = Map<String, dynamic>();
+    data['lat'] = this.lat;
+    data['lng'] = this.lng;
+    return data;
+  }
+}
+
+class Viewport {
+  Location? northeast;
+  Location? southwest;
+
+  Viewport({this.northeast, this.southwest});
+
+  Viewport.fromJson(Map<String, dynamic> json) {
+    northeast = json['northeast'] != null
+        ? Location.fromJson(json['northeast'])
+        : null;
+    southwest = json['southwest'] != null
+        ? Location.fromJson(json['southwest'])
+        : null;
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = Map<String, dynamic>();
+    if (this.northeast != null) {
+      data['northeast'] = this.northeast!.toJson();
+    }
+    if (this.southwest != null) {
+      data['southwest'] = this.southwest!.toJson();
+    }
+    return data;
+  }
+}
+
+class PlusCode {
+  String? compoundCode;
+  String? globalCode;
+
+  PlusCode({this.compoundCode, this.globalCode});
+
+  PlusCode.fromJson(Map<String, dynamic> json) {
+    compoundCode = json['compound_code'];
+    globalCode = json['global_code'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = Map<String, dynamic>();
+    data['compound_code'] = this.compoundCode;
+    data['global_code'] = this.globalCode;
+    return data;
+  }
+}
+
+class Photos {
+  int? height;
+  List<String>? htmlAttributions;
+  String? photoReference;
+  int? width;
+
+  Photos({this.height, this.htmlAttributions, this.photoReference, this.width});
+
+  Photos.fromJson(Map<String, dynamic> json) {
+    height = json['height'];
+    htmlAttributions = json['html_attributions'].cast<String>();
+    photoReference = json['photo_reference'];
+    width = json['width'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = Map<String, dynamic>();
+    data['height'] = this.height;
+    data['html_attributions'] = this.htmlAttributions;
+    data['photo_reference'] = this.photoReference;
+    data['width'] = this.width;
+    return data;
+  }
+}
+
+class OpeningHours {
+  bool? openNow;
+
+  OpeningHours({this.openNow});
+
+  OpeningHours.fromJson(Map<String, dynamic> json) {
+    openNow = json['open_now'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = Map<String, dynamic>();
+    data['open_now'] = this.openNow;
+    return data;
+  }
+}
