0028506bc1844942937ee6e23eb4c68c3e02d7fa
Sourav Bera <sauravberaiit@gmail.com>
2022-04-09 22:48:20 +0530

    modified top header

---
+++ lib/sidebar/components/pricing_cal.dart
@@ -0,0 +1,24 @@
+import 'package:ev_station/sidebar/components/pric_components/price_cal_body.dart';
+import 'package:ev_station/sidebar/components/pric_components/pricing_header.dart';
+import 'package:flutter/material.dart';
+
+class PricingCal extends StatelessWidget {
+  //final String phoneNumber;
+  const PricingCal({
+    Key? key,
+    //required this.phoneNumber,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: Stack(
+        children: const [
+          PricingCalHeader(),
+          BuildPricingCalBody(),
+        ],
+      ),
+    );
+  }
+}
