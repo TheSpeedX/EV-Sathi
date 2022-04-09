904cfe812a26021c6f88cc8b44d1c5ba936d0bf6
pnkr01 <pawankumar49871@gmail.com>
2022-04-09 22:41:04 +0530

    Added OTP Screen

---
+++ lib/screen/auth/widgets/otpscreen.dart
@@ -0,0 +1,52 @@
+import 'package:ev_station/otp/newuser/newuserotp.dart';
+import 'package:flutter/material.dart';
+import '../../../otp/olduserotp.dart';
+
+//TODOS: if we have newUser True --->
+//TODOS: 1. Save user info to firebase.
+//TODOS: 2. Save locally also
+
+//TODOS: if we have newUser False --->
+//TODOS: 1. Retrieve user info from firebase.
+//TODOS: 2. Save locally also.
+
+class OTPScreen extends StatefulWidget {
+  final String phonenumber;
+  final bool isNewUser;
+  // final String userName;
+  static String routename = "/otpscreen";
+  const OTPScreen({
+    Key? key,
+    //required this.userName,
+    required this.phonenumber,
+    required this.isNewUser,
+  }) : super(key: key);
+
+  @override
+  State<OTPScreen> createState() => _OTPScreenState();
+}
+
+class _OTPScreenState extends State<OTPScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: const Color(0xFF091637),
+      body: !widget.isNewUser
+          ? OldUserOTPScreen(
+              phone: widget.phonenumber,
+            )
+
+          //TODOS : CREATE ACCOUNT, IF USER IS NEW..
+
+          : NewUserOTPScreen(
+              phone: widget.phonenumber,
+            ),
+    );
+  }
+}
