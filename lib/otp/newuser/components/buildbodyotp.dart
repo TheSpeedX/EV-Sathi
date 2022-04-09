2e5b0f22a2e72f4e82be9bb2f79840d7c85fc4cf
Sourav Bera <sauravberaiit@gmail.com>
2022-04-09 20:44:17 +0530

    added all imports

---
+++ lib/otp/newuser/components/buildbodyotp.dart
@@ -0,0 +1,10 @@
+import 'dart:async';
+import 'package:ev_station/components/loading_dialog.dart';
+import 'package:ev_station/screen/profile_screen/profile.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:otp_text_field/otp_field.dart';
+import 'package:otp_text_field/style.dart';
+
+import '../../../global.dart';
