930d0b4d5fde58eaba0c775199c737f9976ef74b
TheSpeedX <ggspeedx29@gmail.com>
2022-04-09 17:27:29 +0530

    initial flutter release

---
+++ android/app/src/main/kotlin/com/example/ev_sathi/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ev_sathi
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:ev_station/main.dart';
+import 'package:ev_sathi/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
