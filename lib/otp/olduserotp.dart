5ea68f6db92b11a59006a5a7b7245918504df6a3
Sourav Bera <sauravberaiit@gmail.com>
2022-04-09 20:37:39 +0530

    added OldUserotp Stateful Widget

--- lib/otp/olduserotp.dart
+++ lib/otp/olduserotp.dart
@@ -1,3 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:ev_station/otp/components/buildbodyotp.dart';
 import 'components/headerOtp.dart';
+
+class OldUserOTPScreen extends StatefulWidget {
+  final String phone;
+  const OldUserOTPScreen({
+    Key? key,
+    required this.phone,
+  }) : super(key: key);
