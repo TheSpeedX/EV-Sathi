b7084bb6df9ce362d345c0e774b5c30934171f23
TheSpeedX <ggspeedx29@gmail.com>
2022-04-10 16:50:42 +0530

    add routes

---
+++ lib/routes.dart
@@ -0,0 +1,7 @@
+import 'package:ev_sathi/screen/Splash/splash.dart';
+import 'package:ev_sathi/screen/auth/sign_in/login_form.dart';
+import 'package:flutter/material.dart';
+
+final Map<String, WidgetBuilder> routes = {
+  SplashScreen.routeName : (context) => const SplashScreen(),
+};
